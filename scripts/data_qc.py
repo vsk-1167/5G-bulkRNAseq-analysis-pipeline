2880bd4a16bf0f7fd00a574a45f7d1adb6ca89a1
skvdev17 <skvdev17@cs.washington.edu>
2023-08-09 19:38:04 -0700

    began to utilize argparse

--- scripts/data_qc.py
+++ scripts/data_qc.py
@@ -3,9 +3,36 @@
 #	 data transformations for clustering, and outputs new CSV file with results.
 
 import sys
+import pandas as pd
+import numpy as np
+import argparse
+from pathlib import Path
+
+
+#TODO: Add new data to specified 5G dataset 
+#      --> designated dataset transformation? (avg_stdscaled_log_ratio, etc.) 
+#      --> new experimental condition labels? (no, yes)
+def addData(datasetPath, datasetTransformation, newExpConditions, newDatasetName = "not_specified"):
+   print(datasetPath  + " " + datasetTransformation  + " " + newExpConditions + " " +  newDatasetName)
+
+
+
+#TODO: Analyze entirely new dataset
+
 
 def myfunction(mystring):
     print(mystring)
 
 if __name__ == '__main__':
-    globals()[sys.argv[1]](sys.argv[2])
+   # globals()[sys.argv[1]](sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5] )
+	parser = argparse.ArgumentParser()
+	parser.add_argument("path")
+	args = parser.parse_args()	
+	target_dir = Path(args.path)
+
+	if not target_dir.exists():
+    		print("The target directory doesn't exist")
+    		raise SystemExit(1)
+
+	for entry in target_dir.iterdir():
+    		print(entry.name)
